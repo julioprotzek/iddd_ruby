9eca586d5abb15a52ae90c9a01d4cea7d6c39def
Julio Protzek <julio@flama.is>
2020-12-14 18:20:30 -0300

    ♻️  Refactor tests to use descriptive format

--- test/full_name_test.rb
+++ test/full_name_test.rb
@@ -1,31 +1,31 @@
 require './test/test_helper'
 
-class FullNameTest < Minitest::Test
+class FullNameTest < ActiveSupport::TestCase
   FIRST_NAME = 'Zoe'
   LAST_NAME = 'Doe'
   MARRIED_LAST_NAME = 'Jones-Doe'
   WRONG_FIRST_NAME = 'Zeo'
 
-  def test_changed_first_name
+  test '#with_changed_first_name' do
     name = FullName.new(WRONG_FIRST_NAME, LAST_NAME)
     name = name.with_changed_first_name(FIRST_NAME)
 
     assert_equal FIRST_NAME + ' ' + LAST_NAME, name.as_formatted_name
   end
 
-  def test_changed_last_name
+  test '#with_changed_last_name' do
     name = FullName.new(FIRST_NAME, LAST_NAME)
     name = name.with_changed_last_name(MARRIED_LAST_NAME)
 
     assert_equal FIRST_NAME + ' ' + MARRIED_LAST_NAME, name.as_formatted_name
   end
 
-  def test_formatted_name
+  test '#as_formatted_name' do
     name = FullName.new(FIRST_NAME, LAST_NAME)
     assert_equal FIRST_NAME + ' ' + LAST_NAME, name.as_formatted_name
   end
 
-  def test_first_name_is_required
+  test 'first name is required' do
     error = assert_raises(ArgumentError) do
       FullName.new(nil, LAST_NAME)
     end
@@ -37,21 +37,21 @@ class FullNameTest < Minitest::Test
     assert_equal error.message, 'First name is required.'
   end
   
-  def test_first_name_limit
+  test 'first name length limit' do
     error = assert_raises(ArgumentError) do
       FullName.new('Loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooonnnng', LAST_NAME)
     end
     assert_equal error.message, 'First name must have 50 characters or less.'
   end
 
-  def test_first_name_characters
+  test 'first name format validation' do
     error = assert_raises(ArgumentError) do
       FullName.new('1', LAST_NAME)
     end
     assert_equal error.message, 'First name must be at least one character in length, starting with a capital letter.'
   end
 
-  def test_last_name_is_required
+  test 'last name is required' do
     error = assert_raises(ArgumentError) do
       FullName.new(FIRST_NAME, nil)
     end
@@ -63,14 +63,14 @@ class FullNameTest < Minitest::Test
     assert_equal error.message, 'Last name is required.'
   end
   
-  def test_last_name_limit
+  test 'last name length limit' do
     error = assert_raises(ArgumentError) do
       FullName.new(FIRST_NAME, 'Loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooonnnng')
     end
     assert_equal error.message, 'Last name must have 50 characters or less.'
   end
 
-  def test_last_name_characters
+  test 'last name format validation' do
     error = assert_raises(ArgumentError) do
       FullName.new(FIRST_NAME, '1')
     end
--- test/telephone_test.rb
+++ test/telephone_test.rb
@@ -1,26 +1,26 @@
 require './test/test_helper'
 
-class TelephoneTest < Minitest::Test
-  def test_create_telephone
+class TelephoneTest < ActiveSupport::TestCase
+  test 'telephone has number' do
     phone = Telephone.new('333-550-9010')
     assert_equal phone.number, '333-550-9010'
   end
 
-  def test_number_is_required
+  test 'number is required' do
     error = assert_raises ArgumentError do
       Telephone.new('')
     end
     assert_equal error.message, 'Telephone number is required.' 
   end
 
-  def test_number_length_limit
+  test 'number length limit' do
     error = assert_raises ArgumentError do
       Telephone.new('100200300400500600700800900-100200300400500600700800900')
     end
     assert_equal error.message, 'Telephone number may not be more than 20 characters.' 
   end
 
-  def test_number_format_validation
+  test 'number format validation' do
     phone = Telephone.new('(123) 200-30040')
     assert_equal phone.number, '(123) 200-30040'
 
