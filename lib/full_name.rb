5918b352b746977354b4ea8f07d2a4749e5784b6
Julio Protzek <julio@flama.is>
2020-12-14 19:15:31 -0300

    ♻️  Same naming pattern everywhere

--- lib/full_name.rb
+++ lib/full_name.rb
@@ -3,36 +3,36 @@ class FullName
 
   attr_reader :first_name, :last_name
 
-  def initialize(first_name, last_name)
-    self.first_name = first_name
-    self.last_name = last_name
+  def initialize(a_first_name, a_last_name)
+    self.first_name = a_first_name
+    self.last_name = a_last_name
   end
 
-  def first_name=(first_name)
-    assert_presence(first_name, 'First name is required.')
-    assert_length(first_name, 1, 50, 'First name must have 50 characters or less.')
-    assert(first_name.match?(/[A-Z][a-z]*/), 'First name must be at least one character in length, starting with a capital letter.')
+  def first_name=(a_first_name)
+    assert_presence(a_first_name, 'First name is required.')
+    assert_length(a_first_name, 1, 50, 'First name must have 50 characters or less.')
+    assert(a_first_name.match?(/[A-Z][a-z]*/), 'First name must be at least one character in length, starting with a capital letter.')
 
-    @first_name = first_name
+    @first_name = a_first_name
   end
 
-  def last_name=(last_name)
-    assert_presence(last_name, 'Last name is required.')
-    assert_length(last_name, 1, 50, 'Last name must have 50 characters or less.')
-    assert(last_name.match?(/^[a-zA-Z][ a-zA-Z'-]*[a-zA-Z']?/), 'Last name must be at least one character in length.')
+  def last_name=(a_last_name)
+    assert_presence(a_last_name, 'Last name is required.')
+    assert_length(a_last_name, 1, 50, 'Last name must have 50 characters or less.')
+    assert(a_last_name.match?(/^[a-zA-Z][ a-zA-Z'-]*[a-zA-Z']?/), 'Last name must be at least one character in length.')
 
-    @last_name = last_name
+    @last_name = a_last_name
   end
 
-  def with_changed_first_name(first_name)
-    self.class.new(first_name, @last_name)
+  def with_changed_first_name(a_first_name)
+    self.class.new(a_first_name, last_name)
   end
 
-  def with_changed_last_name(last_name)
-    self.class.new(@first_name, last_name)
+  def with_changed_last_name(a_last_name)
+    self.class.new(first_name, a_last_name)
   end
 
   def as_formatted_name
-    @first_name + ' ' + @last_name
+    first_name + ' ' + last_name
   end
 end
--- lib/telephone.rb
+++ lib/telephone.rb
@@ -3,11 +3,11 @@ class Telephone
 
   attr_reader :number
 
-  def initialize(number)
-    assert_presence(number, 'Telephone number is required.')
-    assert_length(number, 5, 20, 'Telephone number may not be more than 20 characters.')
-    assert(number.match?(/((\(\d{3}\) ?)|(\d{3}-))\d{3}-\d{4}/), 'Telephone number or its format is invalid.')
+  def initialize(a_number)
+    assert_presence(a_number, 'Telephone number is required.')
+    assert_length(a_number, 5, 20, 'Telephone number may not be more than 20 characters.')
+    assert(a_number.match?(/((\(\d{3}\) ?)|(\d{3}-))\d{3}-\d{4}/), 'Telephone number or its format is invalid.')
 
-    @number = number
+    @number = a_number
   end
 end
