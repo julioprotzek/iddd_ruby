4555c4512d583618a62a4d7737fe1208c6982daa
Julio Protzek <julio@flama.is>
2020-12-14 17:18:23 -0300

    🚧 Add number format validation to Telephone

--- lib/telephone.rb
+++ lib/telephone.rb
@@ -5,6 +5,9 @@ class Telephone
 
   def initialize(number)
     assert_argument_presence(number, 'Telephone number is required.')
+    assert_argument_length(number, 5, 20, 'Telephone number may not be more than 20 characters.')
+    assert_argument_true(number.match?(/((\(\d{3}\) ?)|(\d{3}-))\d{3}-\d{4}/), 'Telephone number or its format is invalid.')
+
     @number = number
   end
 end
--- test/telephone_test.rb
+++ test/telephone_test.rb
@@ -6,10 +6,27 @@ class TelephoneTest < Minitest::Test
     assert_equal phone.number, '333-550-9010'
   end
 
-  def test_number_validations
+  def test_number_is_required
     error = assert_raises ArgumentError do
       Telephone.new('')
     end
     assert_equal error.message, 'Telephone number is required.' 
+  end
+
+  def test_number_length_limit
+    error = assert_raises ArgumentError do
+      Telephone.new('100200300400500600700800900-100200300400500600700800900')
+    end
+    assert_equal error.message, 'Telephone number may not be more than 20 characters.' 
+  end
+
+  def test_number_format_validation
+    phone = Telephone.new('(123) 200-30040')
+    assert_equal phone.number, '(123) 200-30040'
+
+    error = assert_raises ArgumentError do
+      Telephone.new('12313123123123')
+    end
+    assert_equal error.message, 'Telephone number or its format is invalid.' 
   end
 end
