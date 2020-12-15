dae91274bc3b264b41406765b91e9d2971c02c15
Julio Protzek <julio@flama.is>
2020-12-15 18:30:05 -0300

    🚧 Add naive implementation for DomainEventPublisher

--- lib/common/domain_event_publisher.rb
+++ lib/common/domain_event_publisher.rb
@@ -1,3 +1,15 @@
 class DomainEventPublisher
   include Singleton
+  
+  def initialize
+    @subscribers = {}
+  end
+  
+  def subscribe(domain_event_klass, &block)
+    @subscribers[domain_event_klass] = block
+  end
+
+  def publish(domain_event)
+    @subscribers[domain_event.class].call(domain_event)
+  end
 end
--- test/common/domain_event_publisher_test.rb
+++ test/common/domain_event_publisher_test.rb
@@ -1,5 +1,30 @@
 require './test/test_helper'
 
+class TestableDomainEvent
+  attr_reader :id, :name
+
+  def initialize(an_id, a_name)
+    @id = an_id
+    @name = a_name
+  end
+end
+
 class DomainEventPublisherTest < ActiveSupport::TestCase
-  
+  setup do
+    @event_handled = false
+  end
+
+  test 'publish' do
+    DomainEventPublisher.instance.subscribe(TestableDomainEvent) do |a_domain_event|
+      assert_equal 123, a_domain_event.id
+      assert_equal 'test', a_domain_event.name
+      @event_handled = true
+    end
+
+    assert_equal false, @event_handled
+
+    DomainEventPublisher.instance.publish(TestableDomainEvent.new(123, 'test'))
+
+    assert_equal true, @event_handled
+  end
 end
