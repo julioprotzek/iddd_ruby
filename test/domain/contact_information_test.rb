fcce24df29143e602b837d85255e4e90ea0fd2b1
Julio Protzek <julio@flama.is>
2020-12-17 16:29:39 -0300

    ♻️  Add IndentityAccessTest to ContactInformationTest

--- test/domain/contact_information_test.rb
+++ test/domain/contact_information_test.rb
@@ -1,33 +1,26 @@
-require './test/test_helper'
+require './test/domain/identity_access_test'
 
-class ContactInformationTest < ActiveSupport::TestCase
-  setup do
-    @contact_information = ContactInformation.new(
-      EmailAddress.new('zoe@example.com'),
-      PostalAddress.new(
-        '123 Pearl Street',
-        'Boulder',
-        'CO',
-        '80301',
-        'US'
-      ),
-      Telephone.new('303-555-1210'),
-      Telephone.new('303-555-1212')
-    )
-  end
+class ContactInformationTest < IdentityAccessTest
+  FIXTURE_POSTAL_ADDRESS = PostalAddress.new(
+    '123 Pearl Street',
+    'Boulder',
+    'CO',
+    '80301',
+    'US'
+  )
 
   test 'valid contact information' do
-    assert_equal EmailAddress.new('zoe@example.com'), @contact_information.email_address
+    assert_equal EmailAddress.new(FIXTURE_USER_EMAIL_ADDRESS), contact_information.email_address
     assert_equal PostalAddress.new(
       '123 Pearl Street',
       'Boulder',
       'CO',
       '80301',
       'US'
-    ), @contact_information.postal_address
+    ), contact_information.postal_address
 
-    assert_equal Telephone.new('303-555-1210'), @contact_information.primary_telephone
-    assert_equal Telephone.new('303-555-1212'), @contact_information.secondary_telephone
+    assert_equal Telephone.new('303-555-1210'), contact_information.primary_telephone
+    assert_equal Telephone.new('777-123-1011'), contact_information.secondary_telephone
   end
 
   test 'validations' do
@@ -37,12 +30,12 @@ class ContactInformationTest < ActiveSupport::TestCase
   end
 
   test 'change email address' do
-    changed_contact_information = @contact_information.change_email_address(EmailAddress.new('johnny@example.com'))
-    assert_equal EmailAddress.new('johnny@example.com'), changed_contact_information.email_address
+    changed_contact_information = contact_information.change_email_address(EmailAddress.new(FIXTURE_USER_EMAIL_ADDRESS_2))
+    assert_equal EmailAddress.new(FIXTURE_USER_EMAIL_ADDRESS_2), changed_contact_information.email_address
   end
 
   test 'change postal address' do
-    changed_contact_information = @contact_information.change_postal_address(PostalAddress.new(
+    changed_contact_information = contact_information.change_postal_address(PostalAddress.new(
       '555 Other Street',
       'Boulder',
       'CO',
@@ -59,66 +52,42 @@ class ContactInformationTest < ActiveSupport::TestCase
   end
 
   test 'change primary telephone' do
-    changed_contact_information = @contact_information.change_primary_telephone(Telephone.new('333-123-0000'))
+    changed_contact_information = contact_information.change_primary_telephone(Telephone.new('333-123-0000'))
     assert_equal Telephone.new('333-123-0000'), changed_contact_information.primary_telephone
   end
 
   test 'change secondary telephone' do
-    changed_contact_information = @contact_information.change_secondary_telephone(Telephone.new('333-123-0000'))
+    changed_contact_information = contact_information.change_secondary_telephone(Telephone.new('333-123-0000'))
     assert_equal Telephone.new('333-123-0000'), changed_contact_information.secondary_telephone
   end
 
   test 'equality' do
     assert_equal ContactInformation.new(
-      EmailAddress.new('zoe@example.com'),
-      PostalAddress.new(
-        '123 Pearl Street',
-        'Boulder',
-        'CO',
-        '80301',
-        'US'
-      ),
+      EmailAddress.new(FIXTURE_USER_EMAIL_ADDRESS),
+      FIXTURE_POSTAL_ADDRESS,
       Telephone.new('303-555-1210'),
       Telephone.new('303-555-1212')
     ), ContactInformation.new(
-      EmailAddress.new('zoe@example.com'),
-      PostalAddress.new(
-        '123 Pearl Street',
-        'Boulder',
-        'CO',
-        '80301',
-        'US'
-      ),
+      EmailAddress.new(FIXTURE_USER_EMAIL_ADDRESS),
+      FIXTURE_POSTAL_ADDRESS,
       Telephone.new('303-555-1210'),
       Telephone.new('303-555-1212')
     )
 
     assert_not_equal ContactInformation.new(
       EmailAddress.new('john@example.com'),
-      PostalAddress.new(
-        '123 Pearl Street',
-        'Boulder',
-        'CO',
-        '80301',
-        'US'
-      ),
+      FIXTURE_POSTAL_ADDRESS,
       Telephone.new('303-555-1210'),
       Telephone.new('303-555-1212')
     ), ContactInformation.new(
-      EmailAddress.new('zoe@example.com'),
-      PostalAddress.new(
-        '123 Pearl Street',
-        'Boulder',
-        'CO',
-        '80301',
-        'US'
-      ),
+      EmailAddress.new(FIXTURE_USER_EMAIL_ADDRESS),
+      FIXTURE_POSTAL_ADDRESS,
       Telephone.new('303-555-1210'),
       Telephone.new('303-555-1212')
     )
 
     assert_not_equal ContactInformation.new(
-      EmailAddress.new('zoe@example.com'),
+      EmailAddress.new(FIXTURE_USER_EMAIL_ADDRESS),
       PostalAddress.new(
         '333 Other Street',
         'Boulder',
@@ -129,38 +98,20 @@ class ContactInformationTest < ActiveSupport::TestCase
       Telephone.new('303-555-1210'),
       Telephone.new('303-555-1212')
     ), ContactInformation.new(
-      EmailAddress.new('zoe@example.com'),
-      PostalAddress.new(
-        '123 Pearl Street',
-        'Boulder',
-        'CO',
-        '80301',
-        'US'
-      ),
+      EmailAddress.new(FIXTURE_USER_EMAIL_ADDRESS),
+      FIXTURE_POSTAL_ADDRESS,
       Telephone.new('(555) 000-1210'),
       Telephone.new('303-555-1212')
     )
 
     assert_not_equal ContactInformation.new(
-      EmailAddress.new('zoe@example.com'),
-      PostalAddress.new(
-        '123 Pearl Street',
-        'Boulder',
-        'CO',
-        '80301',
-        'US'
-      ),
+      EmailAddress.new(FIXTURE_USER_EMAIL_ADDRESS),
+      FIXTURE_POSTAL_ADDRESS,
       Telephone.new('303-555-1210'),
       Telephone.new('(555) 777-1212')
     ), ContactInformation.new(
-      EmailAddress.new('zoe@example.com'),
-      PostalAddress.new(
-        '123 Pearl Street',
-        'Boulder',
-        'CO',
-        '80301',
-        'US'
-      ),
+      EmailAddress.new(FIXTURE_USER_EMAIL_ADDRESS),
+      FIXTURE_POSTAL_ADDRESS,
       Telephone.new('303-555-1210'),
       Telephone.new('303-555-1212')
     )
@@ -169,14 +120,8 @@ class ContactInformationTest < ActiveSupport::TestCase
 
   def assert_validates_presence(attr_name, error_message:)
     args = {
-      email_address: EmailAddress.new('zoe@example.com'),
-      postal_address: PostalAddress.new(
-        '123 Pearl Street',
-        'Boulder',
-        'CO',
-        '80301',
-        'US'
-      ),
+      email_address: EmailAddress.new(FIXTURE_USER_EMAIL_ADDRESS),
+      postal_address: FIXTURE_POSTAL_ADDRESS,
       primary_telephone: Telephone.new('303-555-1210'),
       secondary_telephone: Telephone.new('303-555-1212')
     }
