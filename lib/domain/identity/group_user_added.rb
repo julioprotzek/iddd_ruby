c3fd948815430a3e7fb980469840874ef2196766
Julio Protzek <julio@flama.is>
2020-12-23 16:48:51 -0300

    🐛 Fix GroupUserAdded event

--- lib/domain/identity/group_user_added.rb
+++ lib/domain/identity/group_user_added.rb
@@ -1,9 +1,11 @@
 class GroupUserAdded
-  attr_reader :version, :occurred_at, :username
+  attr_reader :version, :occurred_at, :tenant_id, :group_name, :username
 
-  def initialize(an_username)
+  def initialize(a_tenant_id, a_group_name, an_username)
     @version = 1
     @occurred_at = Time.now
+    @tenant_id = a_tenant_id
+    @group_name = a_group_name
     @username = an_username
   end
 end
