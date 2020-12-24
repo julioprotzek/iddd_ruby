dbec14fb774b7f29ecd4eec5aaff8fa160cdb8d7
Julio Protzek <julio@flama.is>
2020-12-24 16:16:41 -0300

    ✅ Increase Authentication Service test coverage

--- test/domain/identity/authentication_service_test.rb
+++ test/domain/identity/authentication_service_test.rb
@@ -20,4 +20,43 @@ class AuthenticationServiceTest < IdentityAccessTest
     assert_equal user.username, user_descriptor.username
     assert_equal user.person.email_address.address, user_descriptor.email_address
   end
+
+  test 'authentication tenant failure' do
+    user = user_aggregate
+
+    DomainRegistry.user_repository.add(user)
+
+    user_descriptor = DomainRegistry
+      .authentication_service
+      .authenticate(DomainRegistry.tenant_repository.next_identity, user.username, FIXTURE_PASSWORD)
+
+    assert_not_nil user_descriptor
+    assert user_descriptor.null_descriptor?
+  end
+
+  test 'authentication username failure' do
+    user = user_aggregate
+
+    DomainRegistry.user_repository.add(user)
+
+    user_descriptor = DomainRegistry
+      .authentication_service
+      .authenticate(user.tenant_id, FIXTURE_USERNAME_2, FIXTURE_PASSWORD)
+
+    assert_not_nil user_descriptor
+    assert user_descriptor.null_descriptor?
+  end
+
+  test 'authentication password failure' do
+    user = user_aggregate
+
+    DomainRegistry.user_repository.add(user)
+
+    user_descriptor = DomainRegistry
+      .authentication_service
+      .authenticate(user.tenant_id, user.username, FIXTURE_PASSWORD + '-')
+
+    assert_not_nil user_descriptor
+    assert user_descriptor.null_descriptor?
+  end
 end
