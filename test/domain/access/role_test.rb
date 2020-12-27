732c76bef6f0d578308421275ee7cff705988acc
Julio Protzek <julio@flama.is>
2020-12-27 14:10:07 -0300

    ✅ Improve role coverage

--- test/domain/access/role_test.rb
+++ test/domain/access/role_test.rb
@@ -14,4 +14,17 @@ class RoleTest < IdentityAccessTest
     DomainRegistry.role_repository.add(role)
     assert_equal 1, DomainRegistry.role_repository.all_roles(tenant.tenant_id).size
   end
+
+  test 'role uniqueness' do
+    tenant = tenant_aggregate
+    role_1 = tenant.provision_role(name: 'Manager', description: 'A manager role.')
+    DomainRegistry.role_repository.add(role_1)
+
+    error = assert_raise StandardError do
+      role_2 = tenant.provision_role(name: 'Manager', description: 'A manager role.')
+      DomainRegistry.role_repository.add(role_2)
+    end
+
+    assert_equal 'Duplicate Key', error.message
+  end
 end
