2cd6159285e811deb732855063185697acdd57f0
Julio Protzek <julio@flama.is>
2020-12-27 16:35:01 -0300

    ♻️  Better names for adminstrator registerd event properties

--- lib/domain/identity/tenant_administrator_registered.rb
+++ lib/domain/identity/tenant_administrator_registered.rb
@@ -1,15 +1,15 @@
 class TenantAdministratorRegistered
-  attr_reader :version, :occurred_at, :tenant_id, :name, :administrator_name, :email_address, :username, :password
+  attr_reader :version, :occurred_at, :tenant_id, :tenant_name, :administrator_name, :email_address, :username, :temporary_password
 
-  def initialize(tenant_id, name, administrator_name, email_address, username, password)
+  def initialize(tenant_id, tenant_name, administrator_name, email_address, username, temporary_password)
     @version = 1
     @occurred_at = Time.now
 
     @tenant_id = tenant_id
-    @name = name
+    @tenant_name = tenant_name
     @administrator_name = administrator_name
     @email_address = email_address
     @username = username
-    @password = password
+    @temporary_password = temporary_password
   end
 end
