8c8aedb6dffe02a4ed6dc719e0c5547f08d57e7e
Julio Protzek <julio@flama.is>
2020-12-27 12:11:50 -0300

    ✅ Improve group coverage

--- test/domain/identity/group_test.rb
+++ test/domain/identity/group_test.rb
@@ -100,4 +100,17 @@ class GroupTest < IdentityAccessTest
     assert_equal 1, re_grouped.members.size
     assert !re_grouped.member?(user, DomainRegistry.group_member_service)
   end
+
+  test 'remove repository group' do
+    tenant = tenant_aggregate
+    group_a = tenant.provision_group('GroupA', 'A group named GroupA')
+    DomainRegistry.group_repository.add(group_a)
+    not_nil_group = DomainRegistry.group_repository.group_named(tenant.tenant_id, 'GroupA')
+    assert_not_nil not_nil_group
+
+    DomainRegistry.group_repository.remove(group_a)
+    nil_group = DomainRegistry.group_repository.group_named(tenant.tenant_id, 'GroupA')
+
+    assert_nil nil_group
+  end
 end
