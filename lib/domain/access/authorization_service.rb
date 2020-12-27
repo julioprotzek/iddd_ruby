82e5b530aedb57d6c1515549b061fafcff84ce2a
Julio Protzek <julio@flama.is>
2020-12-27 16:25:00 -0300

    ✨ Add ability to verify username authorization by role name

--- lib/domain/access/authorization_service.rb
+++ lib/domain/access/authorization_service.rb
@@ -9,9 +9,18 @@ class AuthorizationService
     @role_repository = role_repository
   end
 
+  def username_in_role?(tenant_id, username, role_name)
+    assert_presence(tenant_id, 'Tenant Id must not be null.')
+    assert_presence(username, 'Username must not be null.')
+    assert_presence(role_name, 'Role name must not be null.')
+
+    user = user_repository.find_by(tenant_id: tenant_id, username: username)
+    user.present? && user_in_role?(user, role_name)
+  end
+
   def user_in_role?(user, role_name)
     assert_presence(user, 'User must not be null.')
-    assert_presence(user, 'Role name must not be null.')
+    assert_presence(role_name, 'Role name must not be null.')
 
     role = role_repository.role_named(user.tenant_id, role_name)
     role.present? && role.in_role?(user, group_member_service)
--- test/domain/access/authorization_service_test.rb
+++ test/domain/access/authorization_service_test.rb
@@ -8,12 +8,30 @@ class AuthorizationServiceTest < IdentityAccessTest
     manager_role = tenant.provision_role(name: 'Manager', description: 'A manager role.', supports_nesting: true)
 
     manager_role.assign_user(user)
+
     DomainRegistry.role_repository.add(manager_role)
 
     is_authorized = DomainRegistry.authorization_service.user_in_role?(user, 'Manager')
     assert is_authorized
 
     is_authorized = DomainRegistry.authorization_service.user_in_role?(user, 'Director')
+    assert !is_authorized
+  end
+
+  test 'username in role authorization' do
+    tenant = tenant_aggregate
+    user = user_aggregate
+    DomainRegistry.user_repository.add(user)
+    manager_role = tenant.provision_role(name: 'Manager', description: 'A manager role.', supports_nesting: true)
+
+    manager_role.assign_user(user)
+
+    DomainRegistry.role_repository.add(manager_role)
+
+    is_authorized = DomainRegistry.authorization_service.username_in_role?(tenant.tenant_id, user.username, 'Manager')
+    assert is_authorized
+
+    is_authorized = DomainRegistry.authorization_service.username_in_role?(tenant.tenant_id, user.username, 'Director')
     assert !is_authorized
   end
 end
