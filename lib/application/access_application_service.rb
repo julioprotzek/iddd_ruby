87227814100637e3bffcbdb3f3ea1ddec2ebb868
Julio Protzek <julio@flama.is>
2020-12-27 19:51:44 -0300

    ✨ Add ability for access application service to verify if user is in role

--- lib/application/access_application_service.rb
+++ lib/application/access_application_service.rb
@@ -16,6 +16,32 @@ class AccessApplicationService
     end
   end
 
+  def user_in_role?(**attrs)
+    user_in_role(**attrs).present?
+  end
+
+  def user_in_role(tenant_id:, username:, role_name:)
+    user_in_role = nil
+
+    tenant_id = TenantId.new(tenant_id)
+    user = user_repository.find_by(tenant_id: tenant_id, username: username)
+
+    if user.present?
+      role = role_repository.role_named(tenant_id, role_name)
+
+      if role.present?
+        group_member_service = GroupMemberService.new(
+          user_repository: user_repository,
+          group_repository: group_repository
+        )
+
+        user_in_role = user if role.in_role?(user, group_member_service)
+      end
+    end
+
+    user_in_role
+  end
+
   private
 
   attr_reader :group_repository, :role_repository, :tenant_repository, :user_repository
--- test/application/access_application_service_test.rb
+++ test/application/access_application_service_test.rb
@@ -20,4 +20,32 @@ class AccessApplicationServiceTest < ApplicationServiceTest
 
     assert role.in_role?(user, DomainRegistry.group_member_service)
   end
+
+  test 'user is in role' do
+    user = user_aggregate
+    DomainRegistry.user_repository.add(user)
+
+    role = role_aggregate
+    DomainRegistry.role_repository.add(role)
+
+    assert !ApplicationServiceRegistry.access_application_service.user_in_role?(
+      tenant_id: user.tenant_id.id,
+      username: user.username,
+      role_name: role.name
+    )
+
+    ApplicationServiceRegistry.access_application_service.assign_user_to_role(
+      AssignUserToRoleCommand.new(
+        tenant_id: user.tenant_id.id,
+        username: user.username,
+        role_name: role.name
+      )
+    )
+
+    assert ApplicationServiceRegistry.access_application_service.user_in_role?(
+      tenant_id: user.tenant_id.id,
+      username: user.username,
+      role_name: role.name
+    )
+  end
 end
