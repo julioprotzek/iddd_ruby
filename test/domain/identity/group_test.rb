c455687698f045364245d0e9e4bd92a1385ec14b
Julio Protzek <julio@flama.is>
2020-12-27 12:08:43 -0300

    ✅ Improve group coverage

--- test/domain/identity/group_test.rb
+++ test/domain/identity/group_test.rb
@@ -82,4 +82,22 @@ class GroupTest < IdentityAccessTest
     assert_equal 0, group_a.members.size
     assert_equal 1, @group_user_removed_count
   end
+
+  test 'remove group referenced user' do
+    tenant = tenant_aggregate
+    group_a = tenant.provision_group('GroupA', 'A group named GroupA')
+    user = user_aggregate
+    DomainRegistry.user_repository.add(user)
+    group_a.add_user(user)
+    DomainRegistry.group_repository.add(group_a)
+
+    assert_equal 1, group_a.members.size
+    assert group_a.member?(user, DomainRegistry.group_member_service)
+    DomainRegistry.user_repository.remove(user)
+
+    re_grouped = DomainRegistry.group_repository.group_named(tenant.tenant_id, 'GroupA')
+    assert_equal 'GroupA', re_grouped.name
+    assert_equal 1, re_grouped.members.size
+    assert !re_grouped.member?(user, DomainRegistry.group_member_service)
+  end
 end
