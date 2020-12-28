216ab2d02bf0c0c3f293f40a471f3064e136a2d6
Julio Protzek <julio@flama.is>
2020-12-28 11:11:49 -0300

    ✨ Add ability for IdentityApplicationService to add_group_to_group

--- lib/application/identity_application_service.rb
+++ lib/application/identity_application_service.rb
@@ -3,6 +3,8 @@ class IdentityApplicationService
     @group_repository = group_repository
     @tenant_repository = tenant_repository
     @user_repository = user_repository
+
+    # IdentityAccessEventProcessor.register
   end
 
   def activate_tenant(command)
@@ -10,16 +12,36 @@ class IdentityApplicationService
     tenant.activate
   end
 
+  def add_group_to_group(command)
+    parent_group = existing_group(command.tenant_id, command.parent_group_name)
+    child_group = existing_group(command.tenant_id, command.child_group_name)
+    parent_group.add_group(child_group, group_member_service)
+  end
+
   def tenant(tenant_id)
     tenant_repository.tenant_of_id(TenantId.new(tenant_id))
+  end
+
+  def group(tenant_id, group_name)
+    group_repository.group_named(TenantId.new(tenant_id), group_name)
   end
 
   private
 
   def existing_tenant(tenant_id)
     tenant = tenant(tenant_id)
-    raise StandardError, 'Tenant does not exist for ' + tenant_id unless tenant.present?
-    tenant
+
+    return tenant if tenant.present?
+
+    raise StandardError, "Tenant does not exist for: #{tenant_id}"
+  end
+
+  def existing_group(tenant_id, group_name)
+    group = group(tenant_id, group_name)
+
+    return group if group.present?
+
+    raise StandardError, "Group does not exist for: #{tenant_id} and: #{group_name}"
   end
 
   def authentication_service
--- test/application/identity_application_service_test.rb
+++ test/application/identity_application_service_test.rb
@@ -16,4 +16,24 @@ class IdentityApplicationServiceTest < ApplicationServiceTest
     assert_equal tenant.name, changed_tenant.name
     assert changed_tenant.active?
   end
+
+  test 'add group to group' do
+    parent_group = group_1_aggregate
+    DomainRegistry.group_repository.add(parent_group)
+
+    child_group = group_2_aggregate
+    DomainRegistry.group_repository.add(child_group)
+
+    assert_equal 0, parent_group.members.size
+
+    ApplicationServiceRegistry.identity_application_service.add_group_to_group(
+      AddGroupToGroupCommand.new(
+        tenant_id: parent_group.tenant_id.id,
+        parent_group_name: parent_group.name,
+        child_group_name: child_group.name
+      )
+    )
+
+    assert_equal 1, parent_group.members.size
+  end
 end
