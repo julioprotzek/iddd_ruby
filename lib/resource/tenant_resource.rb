cba30fd5575888c18faff5384e8e91f9ab215f6f
Julio Protzek <julio@flama.is>
2021-01-16 11:46:19 -0300

    ✨ Add tenant and user resources

---
+++ lib/resource/tenant_resource.rb
@@ -0,0 +1,11 @@
+class TenantResource < AbstractJsonResource
+  get '/tenants/:tenant_id' do
+    tenant = identity_application_service.tenant(params[:tenant_id])
+
+    if tenant.present?
+      tenant.to_json
+    else
+      halt 404, { error: "Tenant not found" }.to_json
+    end
+  end
+end
---
+++ lib/resource/user_resource.rb
@@ -0,0 +1,41 @@
+class UserResource < AbstractJsonResource
+  get '/tenants/:tenant_id/users/:username/authenticate_with/:password' do
+    user_descriptor = identity_application_service.authenticate_user(
+      AuthenticateUserCommand.new(
+        tenant_id: params[:tenant_id],
+        username: params[:username],
+        password: params[:password]
+      )
+    )
+
+    if user_descriptor.null_descriptor?
+      halt 404, { error: "User not authenticated" }.to_json
+    else
+      user_descriptor.user.to_json
+    end
+  end
+
+  get '/tenants/:tenant_id/users/:username' do
+    user = identity_application_service.user(params[:tenant_id], params[:username])
+
+    if user.present?
+      user.to_json
+    else
+      halt 404, { error: "User not found" }.to_json
+    end
+  end
+
+  get '/tenants/:tenant_id/users/:username/in_role/:role_name' do
+    user = access_application_service.user_in_role(
+      tenant_id: params[:tenant_id],
+      username: params[:username],
+      role_name: params[:role_name]
+    )
+
+    if user.present?
+      user.to_json
+    else
+      halt 404, { error: "User not found in role #{params[:role_name]}" }.to_json
+    end
+  end
+end
